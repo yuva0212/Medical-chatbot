07ff5c991410c278fe1cb2d4f430da227bc3f6b3
yuva0212 <149347071+yuva0212@users.noreply.github.com>
2025-03-14 13:44:34 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'yuvasankari',
+    author_email= 'yuvasankari0212@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
